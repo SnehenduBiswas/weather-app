e24a792c08993955f4a3f0db4019691f45c65742
Snehendu Biswas <65412979+SnehenduBiswas@users.noreply.github.com>
2021-11-01 13:56:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const API_key = "ea221bde9cfda936fcbcc49fe4042322";
 class App extends React.Component{
   constructor(){
     super();
-    document.title = "WEATHER";
+    document.title = "WEATHER APP";
     this.state = {
       city:undefined,
       country:undefined,
